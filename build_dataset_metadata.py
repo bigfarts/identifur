1fe088b5fd772cdf957f90a481ff948747dc8a2d
bigfarts <bigfarts@punymail.com>
2022-12-27 13:44:47 -0800

    add option to fetch pending

--- build_dataset_metadata.py
+++ build_dataset_metadata.py
@@ -44,9 +44,9 @@ def main():
     argparser.add_argument("--tag-min-post-count", default=2500, type=int)
     args = argparser.parse_args()
 
-    with sqlite3.connect(f"file:{args.data_db}?mode=ro", uri=True) as db:
-        tags = load_tags(db, args.tag_min_post_count)
-        logging.info("loaded %d tags", len(tags))
+    db = sqlite3.connect(f"file:{args.data_db}?mode=ro", uri=True)
+    tags = load_tags(db, args.tag_min_post_count)
+    logging.info("loaded %d tags", len(tags))
 
     meta_path = os.path.join(args.dataset_path, "_meta")
     try:
@@ -59,25 +59,24 @@ def main():
             f.write(f"{tag}\n")
 
     with open(os.path.join(meta_path, "index"), "wb") as f:
-        with sqlite3.connect(f"file:{args.data_db}?mode=ro", uri=True) as db:
-            db.execute("ATTACH DATABASE ? AS dls", [f"file:{args.dls_db}?mode=ro"])
+        db.execute("ATTACH DATABASE ? AS dls", [f"file:{args.dls_db}?mode=ro"])
 
-            with contextlib.closing(db.cursor()) as cur:
-                cur.execute("SELECT COUNT(*) FROM dls.downloaded")
-                (n,) = cur.fetchone()
+        with contextlib.closing(db.cursor()) as cur:
+            cur.execute("SELECT COUNT(*) FROM dls.downloaded")
+            (n,) = cur.fetchone()
 
-            with contextlib.closing(db.cursor()) as cur:
-                cur.execute(
-                    "SELECT posts.id, posts.tag_string, posts.rating FROM dls.downloaded INNER JOIN posts ON dls.downloaded.post_id = posts.id"
+        with contextlib.closing(db.cursor()) as cur:
+            cur.execute(
+                "SELECT posts.id, posts.tag_string, posts.rating FROM dls.downloaded INNER JOIN posts ON dls.downloaded.post_id = posts.id"
+            )
+            for id, tag_string, rating in tqdm(cur, total=n):
+                post_tags = set(tag_string.split(" "))
+                post_tags.add(f"rating: {rating}")
+
+                f.write(
+                    struct.pack("Q", id)
+                    + bytes(True if tag in post_tags else False for tag in tags)
                 )
-                for id, tag_string, rating in tqdm(cur, total=n):
-                    post_tags = set(tag_string.split(" "))
-                    post_tags.add(f"rating: {rating}")
-
-                    f.write(
-                        struct.pack("Q", id)
-                        + bytes(True if tag in post_tags else False for tag in tags)
-                    )
 
 
 if __name__ == "__main__":
--- fetch_e621_dataset.py
+++ fetch_e621_dataset.py
@@ -57,8 +57,8 @@ async def fetch(session, stats, output_path, db, data_db, id, fetch_full_image):
 
     if resp.status == 404:
         logging.warn("%d not found, skipping", id)
-        db.execute("INSERT INTO visited(post_id) VALUES(?)", [id])
-        db.commit()
+        with db:
+            db.execute("INSERT INTO visited(post_id) VALUES(?)", [id])
         return
 
     resp.raise_for_status()
@@ -72,28 +72,9 @@ async def fetch(session, stats, output_path, db, data_db, id, fetch_full_image):
             f.write(data)
     os.rename(incomplete_path, path)
 
-    db.execute("INSERT INTO downloaded(post_id) VALUES(?)", [id])
-    db.execute("INSERT INTO visited(post_id) VALUES(?)", [id])
-    db.commit()
-
-
-async def worker(session, stats, output_path, db, data_db, queue, fetch_full_image):
-    try:
-        while True:
-            id = await queue.get()
-            await guarded_fetch(
-                session, stats, output_path, db, data_db, id, fetch_full_image
-            )
-            queue.task_done()
-    except asyncio.CancelledError:
-        return
-
-
-async def guarded_fetch(session, stats, output_path, db, data_db, id, fetch_full_image):
-    try:
-        await fetch(session, stats, output_path, db, data_db, id, fetch_full_image)
-    except Exception:
-        logging.exception("failed to download %d", id)
+    with db:
+        db.execute("INSERT INTO downloaded(post_id) VALUES(?)", [id])
+        db.execute("INSERT INTO visited(post_id) VALUES(?)", [id])
 
 
 def init_db(db):
@@ -120,6 +101,7 @@ def init_db(db):
 def rebuild_selected_table(
     db,
     data_db,
+    fetch_pending=False,
     minimum_score=0,
     allowed_ratings="sqe",
     blacklisted_tags=frozenset(),
@@ -131,17 +113,20 @@ def rebuild_selected_table(
 
     with contextlib.closing(data_db.cursor()) as cur:
         cur.execute(
-            "SELECT id, tag_string FROM posts WHERE NOT is_deleted AND NOT is_pending AND score >= ? AND INSTR(?, rating)",
-            [minimum_score, allowed_ratings],
+            "SELECT id, tag_string FROM posts WHERE NOT is_deleted AND (NOT is_pending OR ?) AND score >= ? AND INSTR(?, rating)",
+            [fetch_pending, minimum_score, allowed_ratings],
         )
-        for id, tag_string in tqdm(cur, total=n):
-            tags = tag_string.split(" ")
-            if len(tags) < minimum_tag_count or any(
-                tag in blacklisted_tags for tag in tags
-            ):
-                continue
+        with db:
+            for id, tag_string in tqdm(cur, total=n):
+                tags = tag_string.split(" ")
+                if len(tags) < minimum_tag_count or any(
+                    tag in blacklisted_tags for tag in tags
+                ):
+                    continue
 
-            db.execute("""INSERT OR IGNORE INTO selected(post_id) VALUES(?)""", [id])
+                db.execute(
+                    """INSERT OR IGNORE INTO selected(post_id) VALUES(?)""", [id]
+                )
 
 
 RATINGS = "sqe"
@@ -157,6 +142,7 @@ async def main():
     argparser.add_argument("--num-workers", default=16, type=int)
     argparser.add_argument("--output-path", default="dataset")
     argparser.add_argument("--fetch-full-image", default=False, action="store_true")
+    argparser.add_argument("--fetch-pending", default=False, action="store_true")
     argparser.add_argument("--blacklisted-tags", default="loli,shota")
     argparser.add_argument("--minimum-score", default=0, type=int)
     argparser.add_argument("--minimum-tag-count", default=0, type=int)
@@ -165,85 +151,92 @@ async def main():
 
     queue = asyncio.Queue(args.num_workers)
 
-    with (
-        sqlite3.connect(args.dls_db) as db,
-        sqlite3.connect(f"file:{args.data_db}?mode=ro", uri=True) as data_db,
-    ):
-        init_db(db)
+    db = sqlite3.connect(args.dls_db)
+    data_db = sqlite3.connect(f"file:{args.data_db}?mode=ro", uri=True)
 
-        should_rebuild_selected_table = args.rebuild_selected_table
-        if not should_rebuild_selected_table:
-            with contextlib.closing(db.cursor()) as cur:
-                cur.execute("SELECT NOT EXISTS(SELECT 1 FROM selected)")
-                (should_rebuild_selected_table,) = cur.fetchone()
+    init_db(db)
 
-        if should_rebuild_selected_table:
-            blacklisted_tags = frozenset(
-                args.blacklisted_tags.split(",") if args.blacklisted_tags else []
-            )
-            allowed_ratings = RATINGS[: RATINGS.index(args.maximum_rating) + 1]
-            logging.info(
-                "selected table was either empty or rebuild was requested\nblacklisted tags: %s\nminimum score: %d\nminimum tag count: %d\nallowed ratings: %s",
-                blacklisted_tags,
-                args.minimum_score,
-                args.minimum_tag_count,
-                allowed_ratings,
-            )
-            rebuild_selected_table(
-                db,
-                data_db,
-                args.minimum_score,
-                allowed_ratings,
-                blacklisted_tags,
-                args.minimum_tag_count,
-            )
+    should_rebuild_selected_table = args.rebuild_selected_table
+    if not should_rebuild_selected_table:
+        with contextlib.closing(db.cursor()) as cur:
+            cur.execute("SELECT NOT EXISTS(SELECT 1 FROM selected)")
+            (should_rebuild_selected_table,) = cur.fetchone()
 
-        stats = Stats()
-        async with aiohttp.ClientSession() as session:
-            workers = [
-                asyncio.create_task(
-                    worker(
-                        session,
-                        stats,
-                        args.output_path,
-                        db,
-                        data_db,
-                        queue,
-                        args.fetch_full_image,
-                    )
-                )
-                for _ in range(args.num_workers)
-            ]
+    if should_rebuild_selected_table:
+        blacklisted_tags = frozenset(
+            args.blacklisted_tags.split(",") if args.blacklisted_tags else []
+        )
+        allowed_ratings = RATINGS[: RATINGS.index(args.maximum_rating) + 1]
+        logging.info(
+            "selected table was either empty or rebuild was requested\nfetch pending: %s\nblacklisted tags: %s\nminimum score: %d\nminimum tag count: %d\nallowed ratings: %s",
+            args.fetch_pending,
+            blacklisted_tags,
+            args.minimum_score,
+            args.minimum_tag_count,
+            allowed_ratings,
+        )
+        rebuild_selected_table(
+            db,
+            data_db,
+            args.fetch_pending,
+            args.minimum_score,
+            allowed_ratings,
+            blacklisted_tags,
+            args.minimum_tag_count,
+        )
 
-            async def leader():
-                with contextlib.closing(db.cursor()) as cur:
-                    cur.execute(
-                        "SELECT (SELECT COUNT(*) FROM selected) - (SELECT COUNT(*) FROM visited)"
-                    )
-                    (n,) = cur.fetchone()
+    stats = Stats()
+    async with aiohttp.ClientSession() as session:
 
-                with contextlib.closing(db.cursor()) as cur:
-                    cur.execute(
-                        "SELECT selected.post_id FROM selected LEFT JOIN visited ON selected.post_id = visited.post_id WHERE visited.post_id IS NULL"
-                    )
-
-                    pbar = tqdm(cur, total=n)
-                    for (id,) in pbar:
-                        rate = stats.bytes_received / pbar.format_dict["elapsed"]
-                        pbar.set_postfix(
-                            {
-                                "speed": f"{humanfriendly.format_size(rate, binary=True)}/s"
-                            }
+        async def worker():
+            try:
+                while True:
+                    id = await queue.get()
+                    try:
+                        await fetch(
+                            session,
+                            stats,
+                            args.output_path,
+                            db,
+                            data_db,
+                            id,
+                            args.fetch_full_image,
                         )
-                        pbar.set_description(f"{id}")
-                        await queue.put(id)
+                    except Exception:
+                        logging.exception("failed to download %d", id)
+                    queue.task_done()
+            except asyncio.CancelledError:
+                return
 
-                await queue.join()
+        workers = [asyncio.create_task(worker()) for _ in range(args.num_workers)]
 
-                for worker in workers:
-                    worker.cancel()
+        async def leader():
+            with contextlib.closing(db.cursor()) as cur:
+                cur.execute(
+                    "SELECT (SELECT COUNT(*) FROM selected) - (SELECT COUNT(*) FROM visited)"
+                )
+                (n,) = cur.fetchone()
 
-            await asyncio.gather(*([leader()] + workers))
+            with contextlib.closing(db.cursor()) as cur:
+                cur.execute(
+                    "SELECT selected.post_id FROM selected LEFT JOIN visited ON selected.post_id = visited.post_id WHERE visited.post_id IS NULL"
+                )
+
+                pbar = tqdm(cur, total=n)
+                for (id,) in pbar:
+                    rate = stats.bytes_received / pbar.format_dict["elapsed"]
+                    pbar.set_postfix(
+                        {"speed": f"{humanfriendly.format_size(rate, binary=True)}/s"}
+                    )
+                    pbar.set_description(f"{id}")
+                    await queue.put(id)
+
+            await queue.join()
+
+            for worker in workers:
+                worker.cancel()
+
+        await asyncio.gather(*([leader()] + workers))
 
 
 if __name__ == "__main__":
