4a17bd378fd4abbfec9207b1ae805aaf462d7f74
bigfarts <bigfarts@punymail.com>
2022-12-28 01:40:23 -0800

    allow manual date

--- build_huggingface_dataset.py
+++ build_huggingface_dataset.py
@@ -7,13 +7,16 @@ import re
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("data_db")
+    argparser.add_argument("--date", default=None)
     argparser.add_argument("--dls-db", default="dls.db")
     argparser.add_argument("--images-path", default="images")
     argparser.add_argument("--hub-repo-id", default=None)
     argparser.add_argument("--num-processes", default=1, type=int)
     args = argparser.parse_args()
 
-    date = re.match(r"^data-(\d{4}-\d{2}-\d{2})\.db$", args.data_db).group(1)
+    date = args.date
+    if date is None:
+        date = re.match(r"^data-(\d{4}-\d{2}-\d{2})\.db$", args.data_db).group(1)
 
     repo_id = args.hub_repo_id
     if repo_id is None:
