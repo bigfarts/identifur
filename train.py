d184d1fcf4408f1a8a54fc12e48650906c407603
bigfarts <bigfarts@punymail.com>
2022-12-29 23:22:58 -0800

    add sanity val steps disabler

--- train.py
+++ train.py
@@ -53,6 +53,7 @@ def main():
     argparser.add_argument("--max-epochs", default=10, type=int)
     argparser.add_argument("--num-workers", default=0, type=int)
     argparser.add_argument("--disable-auto-lr-find", default=False, action="store_true")
+    argparser.add_argument("--num-sanity-val-steps", default=2, type=int)
     argparser.add_argument("--tag-min-post-count", default=2500, type=int)
     args = argparser.parse_args()
 
@@ -93,6 +94,7 @@ def main():
     trainer = pl.Trainer(
         auto_lr_find=not args.disable_auto_lr_find,
         max_epochs=args.max_epochs,
+        num_sanity_val_steps=args.num_sanity_val_steps,
         accelerator="gpu",
     )
     trainer.fit(model, dm)
